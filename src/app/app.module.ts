7890bf3dd1b6390f6d057ad823014d8665bcfe47
Jospiado <bugsescripts@hotmail.com>
2022-05-11 19:39:03 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+import { Module2Module } from './module2/module2.module';
+import { Module1Module } from './module1/module1.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    Module1Module,
+    Module2Module,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
